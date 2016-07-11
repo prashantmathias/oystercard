a3291ec4e032be38a92317cb619f2f212f64c63d
Adam <adamjbrowner@mgmail.com>
2016-07-11 17:56:56 +0100

    Completed ch 06

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,5 +1,6 @@
 class Oystercard
   DEFAULT_BALANCE = 0
+  LIMIT = 90
   def initialize
     @balance = DEFAULT_BALANCE
   end
@@ -7,7 +8,11 @@ class Oystercard
   attr_reader :balance
 
   def top_up(money)
+    raise "You can't add money beyond the limit of #{LIMIT}" if full?
     @balance += money
   end
 
+  def full?
+    @balance >= LIMIT
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,14 +7,20 @@ describe Oystercard do
   end
 
   describe "#top_up" do
-    it "adds money to card" do
-      expect(subject.top_up(23)).to eq 23
-    end
-
     it "can top up the balance" do
       expect{ subject.top_up(1) }.to change{ subject.balance }.by 1
     end
+    it "stops the user adding money past the limit" do
+      subject.top_up(90)
+      expect{ subject.top_up(1) }.to raise_error("You can't add money beyond the limit of #{Oystercard::LIMIT}")
+    end
   end
 
+  describe "#full?" do
+    it "checks whether balance limit is reached" do
+      subject.top_up(90)
+      expect(subject.full?).to eq true
+    end
+  end
 
 end
